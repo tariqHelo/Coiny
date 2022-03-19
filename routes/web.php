cf2f6cfbd57c70e92290e877cd91a39e2b51ef4c
unknown <tariq.1994t12@gmail.com>
2022-03-19 20:36:18 +0200

    EditTableStory

--- routes/web.php
+++ routes/web.php
@@ -13,12 +13,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
+
+Route::get('/', function () {
+   return view('layouts.admin');
 })->middleware(['auth'])->name('dashboard');
 
 require __DIR__.'/auth.php';
